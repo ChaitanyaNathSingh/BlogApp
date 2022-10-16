f6b11c1a65a9120220351b21c4ce185c2bba5d8d
ChiatanyaNathSingh <mchaitanyanathsingh@gmail.com>
2022-10-16 22:51:23 +0530

    third changes

--- Blogs/BlogApp/views.py
+++ Blogs/BlogApp/views.py
@@ -11,50 +11,106 @@ from django.shortcuts import get_object_or_404
 @api_view(['GET', 'POST'])
 def posts_create(request):
     if request.method == "GET":
-        p = Post.objects.all()
-        post_serializers = PostSerializers(p, many=True)
-        return Response(post_serializers.data, status=status.HTTP_200_OK)
+        """
+
+        Write your code to list all the POSTS here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # p = Post.objects.all()
+        # post_serializers = PostSerializers(p, many=True)
+        # return Response(post_serializers.data, status=status.HTTP_200_OK)
     else:
-        post_serializers = PostSerializers(data=request.data)
-        post_serializers.is_valid(raise_exception=True)
-        post_serializers.save()
-        return Response(post_serializers.data, status=status.HTTP_201_CREATED)
+        """
+
+        Write your code to post a new POST here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # post_serializers = PostSerializers(data=request.data)
+        # post_serializers.is_valid(raise_exception=True)
+        # post_serializers.save()
+        # return Response(post_serializers.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def posts_update(request, pk):
     p = get_object_or_404(Post, title=pk)
     if request.method == "GET":
-        post_serializers = PostSerializers(p)
-        return Response(post_serializers.data, status=status.HTTP_200_OK)
+        """
+
+        Write your code to get a single POST here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # post_serializers = PostSerializers(p)
+        # return Response(post_serializers.data, status=status.HTTP_200_OK)
     if request.method == "PUT":
-        post_serializers = PostSerializers(instance=p, data=request.data)
-        post_serializers.is_valid(raise_exception=True)
-        post_serializers.save()
-        return Response(post_serializers.data, status=status.HTTP_200_OK)
+        """
+
+        Write your code to update data of single POST here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # post_serializers = PostSerializers(instance=p, data=request.data)
+        # post_serializers.is_valid(raise_exception=True)
+        # post_serializers.save()
+        # return Response(post_serializers.data, status=status.HTTP_200_OK)
     if request.method == "DELETE":
-        p.delete()
-        return Response({'msg': 'done'}, status=status.HTTP_204_NO_CONTENT)
+        """
+
+        Write your code to delete a single POST here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # p.delete()
+        # return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def filter_posts_author(request, pk):
     if request.method == "GET":
-        p = Post.objects.filter(author=pk)
-        post_serializers = PostSerializers(p, many=True)
-        return Response(post_serializers.data, status=status.HTTP_200_OK)
+        """
+        
+        Write your code to filter POSTS by author here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # p = Post.objects.filter(author=pk)
+        # post_serializers = PostSerializers(p, many=True)
+        # return Response(post_serializers.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def filter_posts_genre(request, pk):
     if request.method == "GET":
-        p = Post.objects.filter(genre=pk)
-        post_serializers = PostSerializers(p, many=True)
-        return Response(post_serializers.data, status=status.HTTP_200_OK)
+        """
+        
+        Write your code to filter POSTS by genre here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # p = Post.objects.filter(genre=pk)
+        # post_serializers = PostSerializers(p, many=True)
+        # return Response(post_serializers.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def filter_posts_between_dates(request, start, end):
     if request.method == "GET":
-        p = Post.objects.filter(date__range=[start, end])
-        post_serializers = PostSerializers(p, many=True)
-        return Response(post_serializers.data, status=status.HTTP_200_OK)
+        """
+        
+        Write your code to filter POSTS between dates here and send response status , data
+        Model Name = Post
+        Serializer Class = PostSerializers
+
+        """
+        # p = Post.objects.filter(date__range=[start, end])
+        # post_serializers = PostSerializers(p, many=True)
+        # return Response(post_serializers.data, status=status.HTTP_200_OK)
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializers
